4a68fe0040783c6d0c4031ed706e06adadab7122
Nikhil <nikhilbrijpuriya@gmail.com>
2020-03-08 23:14:17 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const hbs = require("hbs");
+const express = require('express');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Define path for Express config
+const publicDirectoryPath = path.join(__dirname, "./public");
+const viewsPath = path.join(__dirname, "./templates/views");
+const partialsPath = path.join(__dirname, "./templates/partials");
+
+//Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/id', (req, res) => {
+    res.render('id&password')
+})
+
+
+
+app.listen(port, () => {
+    console.log('server is running ' + port)
+})
